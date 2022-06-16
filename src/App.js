a431f27d6cc5355393d2db838743545c4186a2b8
Narayan-Chatalwar <narayanchatalwar@gmail.com>
2022-06-16 21:23:20 +0530

    landing page done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className={styles.uppernotification}>
+        <p>Delivering in Mumbai and Delhi | Same day delivery!</p>
+      </div>
+      <div className={styles.bottomnavbar}>
+        <div className={styles.leftmenu}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Link to="/">SHOP</Link>
+            <span
+              class="material-symbols-outlined"
+              style={{ fontSize: "18px", color: "gray" }}
+            >
+              expand_more
+            </span>
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Link to="/">LEARN</Link>
+            <span
+              class="material-symbols-outlined"
+              style={{ fontSize: "18px", color: "gray" }}
+            >
+              expand_more
+            </span>
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Link to="/">GROW </Link>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/website-logo_400x.png?v=1596288204"
+            alt="logo"
+            width={"135px"}
+            height={"35px"}
+          />
+        </div>
+        <div style={{ display: "flex", gap: "20px" }}>
+         
+          <img
+            src="https://cdn.icon-icons.com/icons2/2642/PNG/512/google_map_location_logo_icon_159350.png"
+            alt="location" width={"25px"}
+          />
+          <span>Mumbai</span>
+          <span class="material-symbols-outlined">search</span>
+          <span class="material-symbols-outlined">person</span>
+          <span class="material-symbols-outlined">local_mall</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
